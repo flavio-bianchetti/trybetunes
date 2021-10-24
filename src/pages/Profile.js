8ac490f1c47ba9a803d5607a259da7296c678d01
Flávio Bianchetti <fjb.developer@gmail.com>
2021-10-23 21:36:01 -0300

    Atendido requisito 13 - validado via npm

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,86 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../component/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../component/Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      name: '',
+      image: '',
+      email: '',
+      description: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  getUserInfo() {
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      await getUser()
+        .then((response) => {
+          this.setState({
+            isLoading: false,
+            name: response.name,
+            image: response.image,
+            email: response.email,
+            description: response.description,
+          });
+        });
+    });
+  }
+
   render() {
+    const {
+      isLoading,
+      name,
+      image,
+      email,
+      description,
+    } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        <div>
+          { isLoading && <Loading /> }
+          {
+            !isLoading
+            && (
+              <div>
+                <img
+                  data-testid="profile-image"
+                  src={ image }
+                  alt={ name }
+                />
+                <p
+                  data-testid="header-user-name"
+                >
+                  { name }
+                </p>
+                <p
+                  data-testid="header-user-email"
+                >
+                  { email }
+                </p>
+                <p>
+                  { description }
+                </p>
+                <Link
+                  to="/profile/edit"
+                >
+                  Editar perfil
+                </Link>
+              </div>
+            )
+          }
+        </div>
       </div>
     );
   }
